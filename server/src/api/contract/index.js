e7939a864e95507812a14ecf7758b62abec70330
Joe Bernitt <j.b5286@gmail.com>
2020-04-03 17:36:53 -0400

    contracts table added

--- server/src/api/contract/index.js
+++ server/src/api/contract/index.js
@@ -1,18 +1,32 @@
 import { initContractEvents } from '@events/contracts';
 
 export const newContract = async (req, res) => {
-  const provider = req.app.get('provider');
+    const provider = req.app.get('provider');
 
-  await initContractEvents(provider, req.contract);
-  res.send({ ok: 'good' });
+    await initContractEvents(provider, req.contract);
+    res.send({ ok: 'good' });
 };
+
+
+export const listContracts = async (req, res) => {
+    let data;
+    let model = models.Contract;
+    try {
+        data = await model.findAll({});
+        return res.send({ function: 'listContracts', data });
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
+};
+
 
 import truffle from './validation/truffle';
 import raw from './validation/raw';
 import embark from './validation/embark';
 
 export const validation = {
-  truffle,
-  raw,
-  embark,
+    truffle,
+    raw,
+    embark,
 };
