f00522b468bf31371df5511b781048e04a634463
Kames <info@kamescg.com>
2020-04-03 09:16:55 -0700

    fixing mege error

--- server/src/lib/events/contracts.js
+++ server/src/lib/events/contracts.js
@@ -3,26 +3,29 @@ import { createEventListener } from './listener';
 import { initEvent } from './event';
 
 export const initContractEvents = async (provider, contractData) => {
+  try {
+    console.log('Initializing Contract...');
+    let contract = await parseJSONToContract(provider, contractData);
+    let contractEvents = Object.keys(contract.interface.events);
 
-    console.log("Initializing Contract...")
-    let contract = await parseJSONToContract(provider, contractData)
-    let contractEvents = Object.keys(contract.interface.events)
+    // initialize event type
+    await initEvent(contract, eventName);
 
-      // initialize event type
-      await initEvent(contract, eventName);
-
-      //KICK OFF EVENT LISTENER HERE
-      let eventABI = contract.interface.events[eventName];
-      console.log('kicking off event listener for ' + eventName);
-      await createEventListener(contract, eventName, eventABI.inputs);
-    }
+    //KICK OFF EVENT LISTENER HERE
+    let eventABI = contract.interface.events[eventName];
+    console.log('kicking off event listener for ' + eventName);
+    await createEventListener(contract, eventName, eventABI.inputs);
   } catch (err) {
     console.error(err);
   }
 };
 
 const parseJSONToContract = (provider, contractData) => {
-    console.log("JOEOE", contractData)
-    const contract = new ethers.Contract(contractData.address, contractData.abi, provider)
-    return contract
-}
+  console.log('JOEOE', contractData);
+  const contract = new ethers.Contract(
+    contractData.address,
+    contractData.abi,
+    provider
+  );
+  return contract;
+};
--- server/src/lib/events/index.js
+++ server/src/lib/events/index.js
@@ -20,28 +20,31 @@ export const initContracts = async (dir, provider) => {
       await initContractEvents(provider, data);
     }
 
-    let files = fs.readdirSync(dir)
+    let files = fs.readdirSync(dir);
     for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        if (/.*.json/.test(file)) {
-            var filename = path.join(dir, file);
-            const fileContents = fs.readFileSync(filename, 'utf8')
-            const data = JSON.parse(fileContents)
-            const contractJSON = await parseTruffleFormatJSON(data, provider)
+      const file = files[i];
+      if (/.*.json/.test(file)) {
+        var filename = path.join(dir, file);
+        const fileContents = fs.readFileSync(filename, 'utf8');
+        const data = JSON.parse(fileContents);
+        const contractJSON = await parseTruffleFormatJSON(
+          data,
+          provider
+        );
 
-            await initContractEvents(provider, contractJSON)
-        }
+        await initContractEvents(provider, contractJSON);
+      }
     }
-}
-
+  }
+};
 
 const parseTruffleFormatJSON = async (json, provider) => {
-    let network = await provider.getNetwork()
-    let chainID = network.chainId
-    let contractJSON = {
-        name: json.name,
-        abi: json.abi,
-        address: json.networks[chainID].address
-    }
-    return contractJSON
-}
+  let network = await provider.getNetwork();
+  let chainID = network.chainId;
+  let contractJSON = {
+    name: json.name,
+    abi: json.abi,
+    address: json.networks[chainID].address,
+  };
+  return contractJSON;
+};
