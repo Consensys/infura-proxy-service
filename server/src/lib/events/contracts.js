6bbfae96092425bfeba3ff0ef50b4e96ad0caf6e
Kames <info@kamescg.com>
2020-04-03 09:21:09 -0700

    ⚡

--- server/src/lib/events/contracts.js
+++ server/src/lib/events/contracts.js
@@ -3,18 +3,22 @@ import { createEventListener } from './listener';
 import { initEvent } from './event';
 
 export const initContractEvents = async (provider, contractData) => {
+  console.log('Initializing Contract...');
+  let contract = await parseJSONToContract(provider, contractData);
+  let contractEvents = Object.keys(contract.interface.events);
+
   try {
-    console.log('Initializing Contract...');
-    let contract = await parseJSONToContract(provider, contractData);
-    let contractEvents = Object.keys(contract.interface.events);
+    for (let i = 0; i < contractEvents.length; i++) {
+      const eventName = contractEvents[i];
 
-    // initialize event type
-    await initEvent(contract, eventName);
+      // initialize event type
+      await initEvent(contract, eventName);
 
-    //KICK OFF EVENT LISTENER HERE
-    let eventABI = contract.interface.events[eventName];
-    console.log('kicking off event listener for ' + eventName);
-    await createEventListener(contract, eventName, eventABI.inputs);
+      //KICK OFF EVENT LISTENER HERE
+      let eventABI = contract.interface.events[eventName];
+      console.log('kicking off event listener for ' + eventName);
+      await createEventListener(contract, eventName, eventABI.inputs);
+    }
   } catch (err) {
     console.error(err);
   }
