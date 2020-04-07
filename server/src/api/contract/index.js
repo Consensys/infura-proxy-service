929d59b1a6b5d80fc412364ff33f9ef52964147a
Kames <info@kamescg.com>
2020-04-07 11:07:28 -0700

    ⚡

--- server/src/api/contract/index.js
+++ server/src/api/contract/index.js
@@ -2,27 +2,27 @@ import { initNewContract } from '@events/contracts';
 import models from '@models';
 
 export const newContract = async (req, res) => {
-    const provider = req.app.get('provider');
+  const provider = req.app.get('provider');
 
-    const FROM_BLOCK = process.env.EVENT_FROM_BLOCK
-        ? parseInt(process.env.EVENT_FROM_BLOCK)
-        : 0;
+  const FROM_BLOCK = process.env.EVENT_FROM_BLOCK
+    ? parseInt(process.env.EVENT_FROM_BLOCK)
+    : 0;
 
-    //TODO add fromBlock parser
-    await initNewContract(provider, req.contract, FROM_BLOCK);
-    res.send({ ok: 'good' }); //TODO
+  //TODO add fromBlock parser
+  await initNewContract(provider, req.contract, FROM_BLOCK);
+  res.status(200).send(); //TODO
 };
 
 export const listContracts = async (req, res) => {
-    let data;
-    let model = models.Contract;
-    try {
-        data = await model.findAll({});
-        return res.send({ function: 'listContracts', data });
-    } catch (error) {
-        console.log(error);
-        return res.send(error);
-    }
+  let data;
+  let model = models.Contract;
+  try {
+    data = await model.findAll({});
+    return res.send({ function: 'listContracts', data });
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
 };
 
 import truffle from './validation/truffle';
@@ -30,7 +30,7 @@ import raw from './validation/raw';
 import embark from './validation/embark';
 
 export const validation = {
-    truffle,
-    raw,
-    embark,
+  truffle,
+  raw,
+  embark,
 };
